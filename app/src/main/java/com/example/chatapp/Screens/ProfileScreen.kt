57eab3c2c86244837d22e9b89b784adab2fa767f
M-AmeenAkbar <145043389+M-AmeenAkbar@users.noreply.github.com>
2024-01-02 21:31:32 +0500

    creating function for showing image from gallery

--- app/src/main/java/com/example/chatapp/Screens/ProfileScreen.kt
+++ app/src/main/java/com/example/chatapp/Screens/ProfileScreen.kt
@@ -4,12 +4,19 @@ import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
@@ -55,6 +62,22 @@ fun ProfileImage(imageUrl: String?, vm: ChatViewModel) {
                 vm.uploadProfileImage(it)
             }
         }
+
+    Box(modifier = Modifier.height(intrinsicSize = IntrinsicSize.Min)) {
+
+        Column(
+            modifier = Modifier
+                .height(8.dp)
+                .fillMaxWidth()
+                .clickable {
+                    launcher.launch("image/*")
+                }, horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Card(shape = CircleShape, modifier = Modifier.padding(8.dp).size(100.dp)) {
+
+            }
+        }
+    }
 
 
 }
