20ebb7ee45d70350e8dae845f69e079ecef645d2
M-AmeenAkbar <145043389+M-AmeenAkbar@users.noreply.github.com>
2024-01-18 21:55:54 +0500

    working on chat screen

--- app/src/main/java/com/example/chatapp/Screens/ChatListScreen.kt
+++ app/src/main/java/com/example/chatapp/Screens/ChatListScreen.kt
@@ -1,13 +1,85 @@
 package com.example.chatapp.Screens
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Add
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.example.chatapp.ChatViewModel
+import com.example.chatapp.ProgressBar
 
 @Composable
 fun ChatListScreen(navController: NavHostController, vm: ChatViewModel) {
+
+    val inProgress = vm.inProcess
+    if (inProgress) {
+        ProgressBar()
+    } else {
+
+    }
+
     BottomNavigationMenu(
         selectedItem = BottomNavigationItem.CHATLIST,
         navController = navController
     )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FAB(
+    showDialog: Boolean,
+    onFabClick: () -> Unit,
+    onDismiss: () -> Unit,
+    onAddChat: (String) -> Unit
+) {
+    val addChatNumber = remember {
+        mutableStateOf("")
+    }
+    if (showDialog) {
+        AlertDialog(
+            onDismissRequest = {
+                onDismiss.invoke()
+                addChatNumber.value = ""
+            },
+            confirmButton = {
+                Button(onClick = {
+                    onAddChat(addChatNumber.value)
+                }) {
+                    Text(text = "Add Chat")
+                }
+            },
+            title = { Text(text = "Add Chat") },
+            text = {
+                OutlinedTextField(
+                    value = addChatNumber.value, onValueChange = {
+                        addChatNumber.value = it
+                    }, keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+                )
+            }
+        )
+        FloatingActionButton(
+            onClick = { onFabClick },
+            containerColor = MaterialTheme.colorScheme.secondary,
+            shape = CircleShape,
+            modifier = Modifier.padding(bottom = 40.dp)
+        ) {
+            Icon(imageVector = Icons.Rounded.Add, contentDescription = null, tint = Color.White)
+        }
+    }
 }
